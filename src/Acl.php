436fe7532101d217ca9165b05b7951f5554fb8ca
ToanDang <dqtoan71@gmail.com>
2018-02-27 17:12:51 +0700

    access_controller

---
+++ src/Acl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 2/27/2018
+ * Time: 5:11 PM
+ */
+namespace toandang\access_controller;
+class Acl
+{
+    public function __construct() {
+
+        echo "hello";
+    }
+}
