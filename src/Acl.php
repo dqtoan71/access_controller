5ce7630f4b7e319591698ecd7b2be25223ccba35
ToanDang <dqtoan71@gmail.com>
2018-02-28 11:25:42 +0700

    access_controller

--- src/Acl.php
+++ src/Acl.php
@@ -8,8 +8,51 @@
 namespace toandang\access_controller;
 class Acl
 {
-    public function __construct() {
+    private $_url;
+    private $_group;
+    private $_accessData = [];
 
-        echo "hello";
+    public function __construct($url = null,$users = null)
+    {
+        $this->_url = $url;
+
+        if (!file_exists($this->_url)) return false;
+
+        $data = json_decode(file_get_contents($this->_url), TRUE);
+
+        if(!$data) return false;
+
+        foreach ($data as $key => $value) {
+
+            if($users === $key){
+
+                $this->_group = $key;
+
+                foreach ($value['allow'] as $controller => $actions) {
+
+                    $this->_accessData[$controller] = $actions[0];
+
+                }
+            }
+        }
+
+        if(!$this->_group) return false; return true;
     }
+
+    public function getGroup(){return $this->_group;}
+
+    public function getAccessData(){return $this->_accessData;}
+
+
+    public function isAllowed($controller, $action)
+    {
+        foreach ($this->_accessData as $con => $act){
+
+            if($con === $controller && $act === $action)return true;
+
+        }
+        return false;
+    }
+
+
 }
