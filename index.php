08a6dcefabffe9dc3c390f37f0dfd34c636a4355
ToanDang <dqtoan71@gmail.com>
2018-02-28 16:22:41 +0700

    access_controller

--- index.php
+++ index.php
@@ -5,3 +5,32 @@
  * Date: 2/27/2018
  * Time: 3:41 PM
  */
+
+
+use toandang\access_controller\Acl;
+include "vendor/autoload.php";
+
+
+$users = "User";
+
+$acl = new Acl('app/config/acl.json',$users);
+
+$group = $acl->getGroup();
+$accessdata = $acl->getAccessData();
+$result =  $acl->isAllowed("login", "login");
+
+
+
+//echo "<pre>";
+//var_dump($group);
+//echo "</pre>";
+//
+//echo "<pre>";
+//var_dump($accessdata);
+//echo "</pre>";
+
+echo "<pre>";
+var_dump($result);
+echo "</pre>";
+
+
